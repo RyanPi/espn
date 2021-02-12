ba904b837da3a51b25a0f7ded370c67fc772c7b7
RyanPi <ryan.pikulski@gmail.com>
2021-02-12 16:53:41 -0500

    Add files via upload

---
+++ espn.py
@@ -0,0 +1,22 @@
+### this is a simple script used to parse espn.com for the top
+### headlines of the day.
+
+import requests
+from bs4 import BeautifulSoup
+
+#get the page and store the page content
+response = requests.get("http://www.espn.com")
+content = response.content
+
+#use BeautifulSoup to load content for parsing
+soup = BeautifulSoup(content, 'html.parser')
+#get the headline header and print the header
+top_headlines = soup.select(".module__header")
+print(top_headlines[0].text +": ")
+#get the headline text and print the text
+headlines = soup.select(".headlineStack__list li a")
+#use a simple incrementer for number formatting
+i=0
+for li in headlines:
+    i+=1
+    print(str(i) + ". " + li.text)
